e0366303307acba1294f904b2bfb85e48ced7b81
Nick Cammorato <nick.cammorato@gmail.com>
2012-07-18 15:20:51 -0400

    Added tests for group, clones, multi-state, and commented out tests for location.

--- tests/resources.pp
+++ tests/resources.pp
@@ -62,4 +62,71 @@ cs_order { 'foo-before-bar':
   first  => 'foo',
   second => 'bar',
   score  => 'INFINITY',
+} ->
+cs_primitive { 'clone':
+  ensure          => present,
+  primitive_class => 'ocf',
+  provided_by     => 'pacemaker',
+  primitive_type  => 'Dummy',
+} ->
+cs_clone { 'clones':
+  ensure           => 'present',
+  primitive        => 'clone',
+  metadata         => {
+    clone-max      => "2",
+    clone-node-max => "1",
+  },
+} ->
+cs_primitive { 'lucas':
+  ensure          => present,
+  primitive_class => 'ocf',
+  provided_by     => 'pacemaker',
+  primitive_type  => 'Dummy',
+} ->
+cs_clone { 'attackofthe':
+  ensure           => 'present',
+  primitive        => 'lucas',
+  metadata         => {
+    clone-max      => "2",
+    clone-node-max => "1",
+  },
+} ->
+cs_group { 'prequel':
+    ensure          => 'present',
+    primitives      => ['attackofthe', 'clones'],
+} ->
+cs_primitive { 'state':
+   ensure          => present,
+  primitive_class => 'ocf',
+  provided_by     => 'pacemaker',
+  primitive_type  => 'Dummy',
+} ->
+cs_ms { 'multi-states':
+    ensure          => present,
+    primitive       => 'state',
+    metadata        => {
+      master-max    => "2",
+      clone-max     => "2",
+      clone-node-max => "1",
+      notify        => "true",
+      target-role   => "Master",
+      is-managed    => "true",
+    }
 }
+# These are commented out for now and just included to illustrate format
+#
+#cs_location { 'no-test-on-node1':
+#  ensure          => 'present',
+#  nodename        => 'node1',
+#  primitive       => 'test',
+#  score           => '-INFINITY',
+#} 
+# 
+#cs_location { 'test_loc_boolean_rule':
+#  operations      => ['#uname eq node1', '#uname eq node2'],
+#  primitive       => 'test',
+#  ensure          => present,
+#  rule            => 'primaryNode-rule',
+#  score           => '1000',
+#  operator        => 'or',
+#}
